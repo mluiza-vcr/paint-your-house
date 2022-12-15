ec8fe55767857c95d5815119275466ca98f6ec11
Malu Rodrigues <maria.luiza@digitalrepublic.com.br>
2022-12-15 17:25:36 -0300

    feat(Main): add BrowserRouter and ThemeProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
-import { ConfigProvider, theme } from "antd";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { ThemeProvider } from "./context/themeContext";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
