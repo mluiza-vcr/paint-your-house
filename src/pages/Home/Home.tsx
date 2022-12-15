9fa9914ff3f914f17c528e4866a98a1c15a2c8ad
Malu Rodrigues <maria.luiza@digitalrepublic.com.br>
2022-12-15 17:27:41 -0300

    feat(Home): create home component

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,17 @@
+import { Button, Divider, Layout, Switch, theme } from "antd";
+
+import LayoutApp from "../../components/Layout";
+
+function Home(): JSX.Element {
+  return (
+    <>
+      <LayoutApp>
+        <div>
+          <Button type="primary"></Button>
+        </div>
+      </LayoutApp>
+    </>
+  );
+}
+
+export default Home;
