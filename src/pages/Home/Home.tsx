e57b343abf9031214f4a724093cfa4856113ce4f
Malu Rodrigues <maria.luiza@digitalrepublic.com.br>
2022-12-19 17:56:15 -0300

    feat(Home): add content example

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -8,6 +8,101 @@ function Home(): JSX.Element {
       <LayoutApp>
         <div>
           <Button type="primary">Home</Button>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, iste
+          eligendi porro velit ut mollitia rem suscipit exercitationem accusamus
+          nihil illum sequi nulla, aperiam consequuntur dolores molestiae
+          debitis? Enim, exercitationem?
+        </div>
+        <div>
+          <Button type="primary">Home</Button>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, iste
+          eligendi porro velit ut mollitia rem suscipit exercitationem accusamus
+          nihil illum sequi nulla, aperiam consequuntur dolores molestiae
+          debitis? Enim, exercitationem?
+        </div>
+        <div>
+          <Button type="primary">Home</Button>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, iste
+          eligendi porro velit ut mollitia rem suscipit exercitationem accusamus
+          nihil illum sequi nulla, aperiam consequuntur dolores molestiae
+          debitis? Enim, exercitationem?
+        </div>
+        <div>
+          <Button type="primary">Home</Button>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, iste
+          eligendi porro velit ut mollitia rem suscipit exercitationem accusamus
+          nihil illum sequi nulla, aperiam consequuntur dolores molestiae
+          debitis? Enim, exercitationem?
+        </div>
+        <div>
+          <Button type="primary">Home</Button>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, iste
+          eligendi porro velit ut mollitia rem suscipit exercitationem accusamus
+          nihil illum sequi nulla, aperiam consequuntur dolores molestiae
+          debitis? Enim, exercitationem?
+        </div>
+        <div>
+          <Button type="primary">Home</Button>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, iste
+          eligendi porro velit ut mollitia rem suscipit exercitationem accusamus
+          nihil illum sequi nulla, aperiam consequuntur dolores molestiae
+          debitis? Enim, exercitationem?
+        </div>
+        <div>
+          <Button type="primary">Home</Button>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, iste
+          eligendi porro velit ut mollitia rem suscipit exercitationem accusamus
+          nihil illum sequi nulla, aperiam consequuntur dolores molestiae
+          debitis? Enim, exercitationem?
+        </div>
+        <div>
+          <Button type="primary">Home</Button>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, iste
+          eligendi porro velit ut mollitia rem suscipit exercitationem accusamus
+          nihil illum sequi nulla, aperiam consequuntur dolores molestiae
+          debitis? Enim, exercitationem?
+        </div>
+        <div>
+          <Button type="primary">Home</Button>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, iste
+          eligendi porro velit ut mollitia rem suscipit exercitationem accusamus
+          nihil illum sequi nulla, aperiam consequuntur dolores molestiae
+          debitis? Enim, exercitationem?
+        </div>
+        <div>
+          <Button type="primary">Home</Button>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, iste
+          eligendi porro velit ut mollitia rem suscipit exercitationem accusamus
+          nihil illum sequi nulla, aperiam consequuntur dolores molestiae
+          debitis? Enim, exercitationem?
+        </div>
+        <div>
+          <Button type="primary">Home</Button>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, iste
+          eligendi porro velit ut mollitia rem suscipit exercitationem accusamus
+          nihil illum sequi nulla, aperiam consequuntur dolores molestiae
+          debitis? Enim, exercitationem?
+        </div>
+        <div>
+          <Button type="primary">Home</Button>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, iste
+          eligendi porro velit ut mollitia rem suscipit exercitationem accusamus
+          nihil illum sequi nulla, aperiam consequuntur dolores molestiae
+          debitis? Enim, exercitationem?
+        </div>
+        <div>
+          <Button type="primary">Home</Button>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, iste
+          eligendi porro velit ut mollitia rem suscipit exercitationem accusamus
+          nihil illum sequi nulla, aperiam consequuntur dolores molestiae
+          debitis? Enim, exercitationem?
+        </div>
+        <div>
+          <Button type="primary">Home</Button>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, iste
+          eligendi porro velit ut mollitia rem suscipit exercitationem accusamus
+          nihil illum sequi nulla, aperiam consequuntur dolores molestiae
+          debitis? Enim, exercitationem?
         </div>
       </LayoutApp>
     </>
